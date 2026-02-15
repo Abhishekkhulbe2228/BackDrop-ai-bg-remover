5a854422747406071edeaa24d99a1371b2ce7ca5
Abhishekkhulbe2228 <abhishekkhulbe2228@gmail.com>
2026-02-15 11:21:59 +0530

    Add navigation bar

---
+++ src/components/Menubar.jsx
@@ -0,0 +1,60 @@
+import {useState} from "react";
+import { assets } from "../assets/assets";
+import { Menu } from "lucide-react";
+
+const Menubar = () => { 
+    const [menuOpen, setMenuOpen] = useState(false);
+    return ( 
+        <nav className="bg-white px-8 py-4 flex justify-between items-center shadow-sm">
+            {/* left side : logo + text */}
+            <div className="flex items-center space-x-3 group">
+                {/* Your logo image with hover effect */}
+                <img 
+                    src={assets.logo} 
+                    alt="SnapClear Logo" 
+                    className="h-10 w-10 object-contain cursor-pointer group-hover:scale-105 transition-transform duration-200 rounded-lg shadow-md"
+                />
+                
+                {/* Brand name with styling */}
+                <span className="text-2xl font-bold cursor-pointer">
+                    <span className="bg-gradient-to-r from-indigo-600 to-purple-600 bg-clip-text text-transparent">
+                        Back
+                    </span>
+                    <span className="text-gray-800">
+                        Drop
+                    </span>
+                </span>
+            </div>
+            {/* right side : Actions buttons */}
+            <div className="hidden md:flex items-center space-x-4">
+                <button className="text-gray-700 hover:text-blue-500 font medium">
+                    Login
+                </button>
+                <button className="bg-gray-100 hover:bg-gray-200 text-gray-700 font-medium px-4 pd-2 rounded-full transition-all">
+                    Sign up
+                </button>
+            </div>
+
+            {/* Mobile Hamburger */}
+            <div className="flex md:hidden">
+                <button onClick={() => setMenuOpen(!menuOpen)}>
+                    {menuOpen ? <X size={28} /> : <Menu size = {28} />}
+                </button>
+            </div>
+
+            {/* Mobile Menu */}
+            {menuOpen && (
+                <div className="absolute top-16 right-8 bg-white shadow-md rounded-md flex flex-col space-y-4 p-4 w-40">
+                    <button className="text-gray-700 hover:text-blue-500 font-medium">
+                        Login
+                    </button>
+                    <button className="bg-gray-100 hover:bg-gray-200 text-gray-700 font-medium px-4 pd-2 rounded-full text-center">
+                        Sign up
+                    </button>
+                </div>
+            )}
+        </nav>
+    )
+}
+
+export default Menubar;
