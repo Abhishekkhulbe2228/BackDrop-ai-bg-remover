ce357b1d1ad567b3a22f7b7c78c6ed1e77e81082
Abhishekkhulbe2228 <abhishekkhulbe2228@gmail.com>
2026-02-20 17:30:49 +0530

    Set up the Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import Footer from "./components/Footer.jsx"
 import Menubar from "./components/Menubar.jsx";
 import Home from "./pages/Home.jsx"
+import {Route, Routes } from "react-router-dom";
 
 const App = () =>  { 
   return ( 
      <div>
-          <Menubar />
-          <Home />
-          <Footer />
+      <Menubar />
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+        </Routes>
+        <Footer />
      </div>
   )
 }
