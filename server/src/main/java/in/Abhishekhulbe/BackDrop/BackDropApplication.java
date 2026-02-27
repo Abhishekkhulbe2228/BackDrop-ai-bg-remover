2ab5c73476b46ae735a03a4b620542be425920ef
Abhishekkhulbe2228 <abhishekkhulbe2228@gmail.com>
2026-02-27 15:17:11 +0530

    Setup full-stack BackDrop project with client and server

---
+++ server/src/main/java/in/Abhishekhulbe/BackDrop/BackDropApplication.java
@@ -0,0 +1,13 @@
+package in.Abhishekhulbe.BackDrop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackDropApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackDropApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/in/Abhishekhulbe/BackDrop/BackDropApplicationTests.java
@@ -0,0 +1,13 @@
+package in.Abhishekhulbe.BackDrop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackDropApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
